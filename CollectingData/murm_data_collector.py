43fc6519c48fe2e58661ccac133031b82a8b62fa
Jang <87692888+JangSeongwon@users.noreply.github.com>
2022-12-30 14:02:40 +0900

    Update murm_data_collector.py

--- CollectingData/murm_data_collector.py
+++ CollectingData/murm_data_collector.py
@@ -13,15 +13,21 @@ import argparse
 physicsClient = p.connect(p.GUI)
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--name", type=str, default='jang')
-parser.add_argument("--num_trajectories", type=int, default=20)
-parser.add_argument("--num_timesteps", type=int, default=170) # 450 Making videos For Graduation exam #170 for 9 boxes env
+parser.add_argument("--name", type=str, default='singleview')
+# parser.add_argument("--name", type=str, default='multiview1')
+# parser.add_argument("--name", type=str, default='multiview2')
+parser.add_argument("--num_episodes", type=int, default=300) # Analogies made 50K transitions (300 episodes=60K T) "--num_trajectories"
+parser.add_argument("--num_timesteps", type=int, default=300) # 450 Making videos For Graduation exam #250 for 9 boxes env
+
+# 185 timestep, 300 epi == 55k transitions
+
 parser.add_argument("--video_save", type=int, default=1, help="Set to zero for no video saving")
 args = parser.parse_args()
 
 path = "/home/jang/data/data_collection/dataset/"
 demo_data_save_path = path + args.name + "_demos"
-recon_data_save_path = path + args.name + "_images.npy"
+gImage_save_path = path + args.name + "_global_images.npy"
+aImage_save_path = path + args.name + "_active_images.npy"
 
 a = roboverse.register_bullet_environments
 
@@ -45,31 +51,33 @@ act_dim = env.action_space.shape[0]
 # print(act_dim)
 num_datasets = 0
 demo_dataset = []
-image_dataset = {
-    'observations': np.zeros((args.num_trajectories, args.num_timesteps, imlength), dtype=np.uint8),
+Global_image_dataset = {
+    'observations': np.zeros((args.num_episodes, args.num_timesteps, imlength), dtype=np.uint8),
     # Saved in [[[ img_in_num (imlength)] *timestep] *num of trajectories]
-    'object': [],
-    'env': np.zeros((args.num_trajectories, imlength), dtype=np.uint8),
+    # 'object': [],
+    'initial env': np.zeros((args.num_episodes, imlength), dtype=np.uint8),
+    # Saving initial obs of the environment for each trajectory
+}
+Active_image_dataset = {
+    'observations': np.zeros((args.num_episodes, args.num_timesteps, imlength), dtype=np.uint8),
+    # Saved in [[[ img_in_num (imlength)] *timestep] *num of trajectories]
+    # 'object': [],
+    'initial env': np.zeros((args.num_episodes, imlength), dtype=np.uint8),
     # Saving initial obs of the environment for each trajectory
 }
 
 avg_tasks_done = 0
-for j in tqdm(range(args.num_trajectories)):
+for j in tqdm(range(args.num_episodes)):
     env.demo_reset()
     # Initial Image Saving
-    image_dataset['env'][j, :] = np.uint8(env.render_obs().transpose()).flatten()
-    # Object info Saving
-    image_dataset['object'].append(env.curr_object)
+    Global_image_dataset['initial env'][j, :] = np.uint8(env.render_obs().transpose()).flatten()
+    Active_image_dataset['initial env'][j, :] = np.uint8(env.render_obs_active().transpose()).flatten()
 
     trajectory = {
         'observations': [],
         'next_observations': [],
         'actions': np.zeros((args.num_timesteps, act_dim), dtype=np.float64),
         'rewards': np.zeros((args.num_timesteps), dtype=np.float64),
-        'terminals': np.zeros((args.num_timesteps), dtype=np.uint8),
-        'agent_infos': np.zeros((args.num_timesteps), dtype=np.uint8),
-        'env_infos': np.zeros((args.num_timesteps), dtype=np.uint8),
-        #'object_name': env.curr_object,
     }
 
     images = []
@@ -79,15 +87,16 @@ for j in tqdm(range(args.num_trajectories)):
         img = np.uint8(env.render_obs())
         img_active = np.uint8(env.render_obs_active())
         # All images Savings
-        image_dataset['observations'][j, i, :] = img.transpose().flatten()
+        Global_image_dataset['observations'][j, i, :] = img.transpose().flatten()
+        Active_image_dataset['observations'][j, i, :] = img_active.transpose().flatten()
 
         observation = env.get_observation()
 
         action = env.get_demo_action()
         #print('Saving Action', action)
         next_observation, reward, done, info = env.step(action)
+        #print('reward savings = -1', reward)
 
-        #print('reward savings', reward)
         # Obs before action
         trajectory['observations'].append(observation)
         # Action given as delta_pos
@@ -96,19 +105,20 @@ for j in tqdm(range(args.num_trajectories)):
         trajectory['next_observations'].append(next_observation)
         trajectory['rewards'][i] = reward
 
-        # Checking with videos (Global & Active)
-        if args.video_save:
-            img = env.render_obs()
-            images.append(img)
-        if args.video_save:
-            img_active = env.render_obs_active()
-            images_active.append(img_active)
+        #print('Checked to be 0 when success'.reward)
 
+        #Checking with videos (Global & Active)
+        # if args.video_save:
+        #     img = env.render_obs()
+        #     images.append(img)
+        # if args.video_save:
+        #     img_active = env.render_obs_active()
+        #     images_active.append(img_active)
+    print('reward', reward)
     demo_dataset.append(trajectory)
     avg_tasks_done += env.done
-    print()
 
-    if ((j + 1) % 4) == 0:
+    if ((j + 1) % 500) == 0:
         curr_name = demo_data_save_path + '_{0}.pkl'.format(num_datasets)
         file = open(curr_name, 'wb')
         pkl.dump(demo_dataset, file)
@@ -117,11 +127,12 @@ for j in tqdm(range(args.num_trajectories)):
         num_datasets += 1
         demo_dataset = []
 
-    if args.video_save:
-        roboverse.utils.save_video('{}/{}_global.avi'.format(path, j), images)
-    if args.video_save:
-        roboverse.utils.save_video('{}/{}_active.avi'.format(path, j), images_active)
+    # if args.video_save:
+    #     if j % 10 == 0:
+    #         roboverse.utils.save_video('{}/{}_global.avi'.format(path, j), images)
+    #         roboverse.utils.save_video('{}/{}_active.avi'.format(path, j), images_active)
 
-print('Success Rate: {}'.format(avg_tasks_done / args.num_trajectories))
-np.save(recon_data_save_path, image_dataset)
+print('Demo Success Rate: {}'.format(avg_tasks_done / args.num_episodes))
+np.save(gImage_save_path, Global_image_dataset)
+np.save(aImage_save_path, Active_image_dataset)
 
