00ff97471af0b70f6d99ef6b806112224759b5da
Jang <87692888+JangSeongwon@users.noreply.github.com>
2023-02-09 22:55:34 +0900

    Update image_viewing.py

--- Training/RL/image_viewing.py
+++ Training/RL/image_viewing.py
@@ -2,8 +2,10 @@
 import numpy as np
 from PIL import Image
 
-img_array = np.load('/media/jang/jang/0ubuntu/image_dataset/Images_produced_for_goals/4.npy')
+img_array = np.load('/media/jang/jang/0ubuntu/image_dataset/Images_produced_for_goals/m1_4.npy')
 
 im = Image.fromarray(img_array.astype(np.uint8))
 
 im.show()
+
+#envs as m1 / m2 / m2
