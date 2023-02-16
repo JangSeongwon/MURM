7ae877193fc72a122b1a05977a636903465224b3
Jang <87692888+JangSeongwon@users.noreply.github.com>
2023-02-16 16:01:22 +0900

    Update murm_images_collector.py

--- CollectingData/murm_images_collector.py
+++ CollectingData/murm_images_collector.py
@@ -12,19 +12,19 @@ import argparse
 
 parser = argparse.ArgumentParser()
 #parser.add_argument("--name", type=str, default='singleview')
-parser.add_argument("--name", type=str, default='multiview1')
-# parser.add_argument("--name", type=str, default='multiview2')
-parser.add_argument("--num_episodes", type=int, default=300)
-parser.add_argument("--num_timesteps", type=int, default=275)
+# parser.add_argument("--name", type=str, default='wall')
+parser.add_argument("--name", type=str, default='randombox')
+parser.add_argument("--num_episodes", type=int, default=900)
+parser.add_argument("--num_timesteps", type=int, default=250)
 parser.add_argument("--video_save", type=int, default=1, help="Set to zero for no video saving")
 args = parser.parse_args()
 
-path = "/media/jang/jang/0ubuntu/image_dataset/"
-gImage_save_path = path + args.name + "_global_images.npy"
+path = "/media/jang/jang/0ubuntu/image_dataset/64/randombox/"
+# gImage_save_path = path + args.name + "_global_images.npy"
 aImage_save_path = path + args.name + "_active_images.npy"
 
 a = roboverse.register_bullet_environments
-state_env = roboverse.make('MURMENV-v1', object_subset='test')
+state_env = roboverse.make('MURMENV-v2', object_subset='test')
 imsize = state_env.obs_img_dim
 
 renderer_kwargs=dict(
@@ -61,23 +61,18 @@ Global_image_dataset, Active_image_dataset = image_data()
 for j in tqdm(range(args.num_episodes)):
 
     env.demo_reset()
-    images = []
+    # images = []
     images_active = []
 
     for i in range(args.num_timesteps):
-        img = np.uint8(env.render_obs())
+        # img = np.uint8(env.render_obs())
         img_active = np.uint8(env.render_obs_active())
         # All images Savings
-        Global_image_dataset['observations'][j, i, :] = img.transpose().flatten()
+        # Global_image_dataset['observations'][j, i, :] = img.transpose().flatten()
         Active_image_dataset['observations'][j, i, :] = img_active.transpose().flatten()
 
         action = env.get_demo_action()
         next_observation, reward, done, info = env.step(action)
-
-    if reward == -1:
-        print('reward', reward)
-    avg_tasks_done += env.done
-
         #Checking with videos (Global & Active)
         # if args.video_save:
         #     img = env.render_obs()
@@ -86,9 +81,13 @@ for j in tqdm(range(args.num_episodes)):
         #     img_active = env.render_obs_active()
         #     images_active.append(img_active)
 
+    if reward == -1:
+        print('reward', reward)
+    avg_tasks_done += env.done
+
     # roboverse.utils.save_video('{}/{}_global.avi'.format(path, j), images)
     # roboverse.utils.save_video('{}/{}_active.avi'.format(path, j), images_active)
 
 print('Demo Success Rate: {}'.format(avg_tasks_done / args.num_episodes))
-np.save(gImage_save_path, Global_image_dataset)
+# np.save(gImage_save_path, Global_image_dataset)
 np.save(aImage_save_path, Active_image_dataset)
