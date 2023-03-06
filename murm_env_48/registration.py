d0583f8a81eb33dfcdf094e3b167dac72679e986
Jang <87692888+JangSeongwon@users.noreply.github.com>
2023-03-06 21:57:16 +0900

    Add files via upload

---
+++ murm_env_48/registration.py
@@ -0,0 +1,39 @@
+import gym
+
+BULLET_ENVIRONMENT_SPECS = (
+    {
+        'id': 'MURMENV-v1',
+        'entry_point': ('roboverse.envs.murm_env:MURMENV'),
+    },
+
+
+{
+        'id': 'MURMENV-v2',
+        'entry_point': ('roboverse.envs.murm_env_v2:MURMENV_v2'),
+    },
+
+{
+        'id': 'MURMENV-v3',
+        'entry_point': ('roboverse.envs.murm_env_v3:MURMENV_v3'),
+    },
+{
+        'id': 'MURMENV-v4',
+        'entry_point': ('roboverse.envs.murm_env_v4:MURMENV_v4'),
+    },
+
+
+
+)
+
+def register_bullet_environments():
+    for bullet_environment in BULLET_ENVIRONMENT_SPECS:
+        gym.register(**bullet_environment)
+
+    gym_ids = tuple(
+        environment_spec['id']
+        for environment_spec in BULLET_ENVIRONMENT_SPECS)
+    return gym_ids
+
+def make(env_name, *args, **kwargs):
+    env = gym.make(env_name, *args, **kwargs)
+    return env
